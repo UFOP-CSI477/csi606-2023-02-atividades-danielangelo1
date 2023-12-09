3c15f461e3416f1c3a2ef5ca7aec2bfb6f798579
Daniel <danielangelo1234@gmail.com>
2023-12-09 20:27:08 -0300

    feat: finish extra exercise 4

--- Atividades/atividades-extras/exercicio_4/js/index.js
+++ Atividades/atividades-extras/exercicio_4/js/index.js
@@ -1,45 +1,52 @@
 const getRepository = async (user) => {
-    /* 
+  /* 
         Esta função vai faz um request na API aberta do github
         e retorna um array contendo as seguintes informações do usuário
         login, email, public_repos, followers, following
     */
 
-    try{
-        const url = `https://api.github.com/users`;
-        const data = await fetch(`${url}/${user}`)
-            .then((data) => data.json())
-            .catch((err) => err.json());
-        const { login, email, public_repos, followers, following } = data;
-        return [login, email, public_repos, followers, following] ;
-    } catch(e) {
-        console.error(e)
-    }   
+  try {
+    const url = `https://api.github.com/users`;
+    const data = await fetch(`${url}/${user}`)
+      .then((data) => data.json())
+      .catch((err) => err.json());
+    const { login, email, public_repos, followers, following } = data;
+    return [login, email, public_repos, followers, following];
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 const showError = (show) => {
-    /*
-        Criar uma função que mostre um erro ao usuário
-        quando o usuário pesquisado não existir 
-    */
-}
+  const error = document.getElementById("disable_text");
+  error.hidden = false;
+  if (show) {
+    error.hidden = true;
+  }
+};
 
 const insertRow = async () => {
-    //Recupere o input digitado
-    var nameValue = 
+  //Recupere o input digitado
+  var nameValue = document.getElementById("github_name").value;
 
-    const userInfos = await getRepository(nameValue)
-    
-    /*  
+  const userInfos = await getRepository(nameValue);
+
+  /*  
         Se o elemento login retornado pela função getRepository 
         for undefined, significa que o usuário não existe, então será
         mostrado ao usuário uma mensagem de erro
     */
-    if(!userInfos[0]){
-        showError(false)
-        return true
-    }
+  if (!userInfos[0]) {
+    showError(false);
+    return true;
+  }
 
-    //adicione o usuário digitado na tabela de id "myTable"
+  //adicione o usuário digitado na tabela de id "myTable"
+  const table = document.getElementById("myTable");
+  const row = table.insertRow(1);
 
-}
+  userInfos.forEach((info, index) => {
+    const cell = row.insertCell(index);
+    cell.innerHTML = info || "Não informado";
+  });
+};
