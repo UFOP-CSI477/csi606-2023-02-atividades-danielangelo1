47b42c6361ce6b450f693128163d0398a14e21af
Daniel <danielangelo1234@gmail.com>
2023-12-09 16:17:41 -0300

    fix: correct donation url

--- Atividades/atividade-pratica-01/src/app/routes/donationRoutes.js
+++ Atividades/atividade-pratica-01/src/app/routes/donationRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const DonationController = require("../controllers/donationController");
 
-router.get("/", DonationController.index);
-router.get("/:id", DonationController.show);
-router.get("/person/:person_id", DonationController.showByPerson);
-router.get("/local/:local_id", DonationController.showByLocal);
-router.post("/", DonationController.store);
-router.put("/:id", DonationController.update);
-router.delete("/:id", DonationController.destroy);
+router.get("/donations/", DonationController.index);
+router.get("/donations/:id", DonationController.show);
+router.get("/donations/person/:person_id", DonationController.showByPerson);
+router.get("/donations/local/:local_id", DonationController.showByLocal);
+router.post("/donations/", DonationController.store);
+router.put("/donations/:id", DonationController.update);
+router.delete("/donations/:id", DonationController.destroy);
 
 module.exports = router;
