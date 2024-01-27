5fd8b81cc3cfe621e02ae8935f0fcae682e20885
Daniel <danielangelo1234@gmail.com>
2024-01-27 20:15:56 -0300

    Add donation appointment functionality

--- Atividades/atividade-pratica-02/web/src/pages/marcacoes/Marcacoes.tsx
+++ Atividades/atividade-pratica-02/web/src/pages/marcacoes/Marcacoes.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import Footer from "../../components/footer/Footer";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import axios from "axios";
 import DonationItem from "../../components/donationItem/DonationItem";
+import Footer from "../../components/footer/Footer";
 
 interface BloodDonation {
   person_id: number;
@@ -13,6 +13,14 @@ const API_URL = "http://localhost:3000/donations/";
 
 const Marcacoes = () => {
   const [donations, setDonations] = useState<BloodDonation[]>([]);
+  const [newDonationAppointment, setNewDonationAppointment] =
+    useState<BloodDonation>({
+      person_id: 0,
+      local_id: 0,
+      data: "",
+    });
+  const [editingDonations, setEditingDonations] =
+    useState<BloodDonation | null>(null);
 
   useEffect(() => {
     fetchDonations();
@@ -28,6 +36,55 @@ const Marcacoes = () => {
       }
     } catch (error) {
       console.log("Erro na requisição", error);
+    }
+  };
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setNewDonationAppointment({
+      ...newDonationAppointment,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleAddDonation = async () => {
+    try {
+      if (editingDonations) {
+        await axios.put(
+          `${API_URL}/${editingDonations.person_id}`,
+          newDonationAppointment,
+        );
+        setDonations((prevDonations) =>
+          prevDonations.map((donation) =>
+            donation.person_id === editingDonations.person_id
+              ? { ...donation, ...newDonationAppointment }
+              : donation,
+          ),
+        );
+        setEditingDonations(null);
+      } else {
+        const response = await axios.post(API_URL, newDonationAppointment);
+        setDonations((prevDonations) => [...prevDonations, response.data]);
+      }
+      setNewDonationAppointment({ person_id: 0, local_id: 0, data: "" });
+    } catch (error) {
+      console.log("error na requisição", error);
+    }
+  };
+
+  const handleEditDonation = (donation: BloodDonation) => {
+    setEditingDonations(donation);
+  };
+
+  const handleDeleteDonation = async (person_id: number) => {
+    try {
+      await axios.delete(`${API_URL}/${person_id}`);
+      setDonations(
+        donations.filter((donation) => donation.person_id !== person_id),
+      );
+    } catch (error) {
+      console.error("Error ao deletar o post", error);
     }
   };
 
