d865b5b4d4951ccf2c33fc04d209a1ada32f47fc
Daniel <danielangelo1234@gmail.com>
2024-01-28 20:32:02 -0300

    Add city list and fetch functionality

--- Atividades/atividade-pratica-02/web/src/pages/cidades/Cidades.tsx
+++ Atividades/atividade-pratica-02/web/src/pages/cidades/Cidades.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
+import axios from "axios";
+import cityDialog from "../../components/cityDialog/CityDialog";
+
+interface CityProps {
+  name: string;
+  estado_id: number;
+}
+
+const API_URL = "http://localhost:3000/city";
 
 const Cidades = () => {
+  const [cities, setCities] = useState<CityProps[]>([]);
+
+  useEffect(() => {
+    fetchCities();
+  }, []);
+
+  const fetchCities = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setCities(response.data);
+    } catch (error) {
+      console.error("Error na requisição", error);
+    }
+  };
+
   return (
     <>
       <Header />
-      <p>Cidades</p>
+      <main>
+        <ul>
+          {cities.map((city) => (
+            <li key={city.estado_id}>{city.name}</li>
+          ))}
+        </ul>
+        <CityDialog />
+      </main>
       <Footer />
     </>
   );
